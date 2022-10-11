fc5ce78f309ff986a7effccef89f05d4c12797c3
+ <+>
2022-10-11 14:01:53 +0300

    add changes

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,65 +1,49 @@
 import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
-import BotSpecs from "./BotSpecs";
 
 function BotsPage() {
-  //start here with your code for step one
-  const [bots, setBots]= useState([]);
-  const [selectedBot, setSelectBot] = useState(null);
-  const [botsArmy, setBotsArmy]= useState([]);
+  
+  const [botsPage, setBotsPage] = useState([])
+  const [botsArmy, setBotsArmy] = useState([])
+  const [monitorCollection, setMonitorCollection] = useState(false)
 
-useEffect(() => {
+  useEffect(() => {
     fetch("http://localhost:8002/bots")
     .then((res) => res.json())
     .then((bots) => {
-      setBots(bots)
+      setBotsPage(bots)
     })
-  }, [selectedBot])
+  }, [monitorCollection])
 
+  function addToBotArmy(bot) {
+    const updatedArmy = botsArmy.find((robot) => robot.id === bot.id)
+    if (!updatedArmy) setBotsArmy([...botsArmy, bot])
 
-const selectBot = (bot) =>{
-  const addToArmy = bots.filter((bort)=> bort.id === bot.id);
-  const botIsAvailable = botsArmy.find((bort) => bort.id === bot.id);
-     
-  setSelectBot(addToArmy[0]);
-
-};
-
-const enlistBot = (bot) =>{
-  const addToArmy = bots.find((bort) =>bort.id ===bot.id);
-  const botIsAvailable = botsArmy.filter((bort) => bort.id === bot.id);
-  if(!botIsAvailable){
-          setBotsArmy([...botsArmy, addToArmy[0]]);
   }
-};
 
-const removeBotFromArmy = async(bot) =>{
-  try {
-			  const res = await fetch(`http://localhost:8002/bots/${bot}`, {
-				      method: "DELETE",
-			});
-			const removeBotFromArmy = botsArmy.filter((bort) => bort.id !== bot);
-			setBotsArmy(removeBotFromArmy);
-		} catch (error) {
-			console.log(error);
-		}
-};
+  function removeBotFromArmy(bot) {
+    const updatedArmy = botsArmy.find((robot) => robot.id === bot.id)
+    if (updatedArmy) {
+      setBotsArmy(
+        botsArmy.filter((robots) => robots.id !== bot.id)
+      )
+    }
+  }
 
-const resetSelectedBot = () => {
-		setSelectBot(null);
-	}; 
-
+  function deleteBotFromCollection(bot) {
+    fetch(`http://localhost:8002/bots/${bot.id}`, {
+      method: "DELETE",
+    })
+    setMonitorCollection(true)
+}
 
   return (
     <div>
-      <YourBotArmy army={botsArmy} removeBotFromArmy={removeBotFromArmy} selectBot={selectBot} />
-      {!selectedBot && <BotCollection bots={bots} selectBot={selectBot} />}
-      {selectedBot && (
-				<BotSpecs enlistBot={enlistBot} resetSelectedBot={resetSelectedBot} bot={selectedBot} />
-        )}
+      <YourBotArmy botsArmy={botsArmy} onRemoveBotFromArmy={removeBotFromArmy}/>
+      <BotCollection botsPage={botsPage} onAddBotArmy={addToBotArmy} onDeleteBotFromCollection = {deleteBotFromCollection}/>
     </div>
-  );
+  )
 }
 
-export default BotsPage;
+export default BotsPage
